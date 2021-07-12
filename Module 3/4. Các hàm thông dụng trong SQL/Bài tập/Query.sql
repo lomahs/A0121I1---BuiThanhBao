e46ae96358718b7ff51f45b29e1cfba4a1698a62
Lomahs <thanh.bui.bao@gmail.com>
2021-07-12 16:21:00 +0700

    Updated Chap 3,4

--- Module 3/4. Các hàm thông dụng trong SQL/Bài tập/Query.sql
+++ Module 3/4. Các hàm thông dụng trong SQL/Bài tập/Query.sql
@@ -9,6 +9,13 @@ CREATE TABLE Fee
     amount int
 );
 
+INSERT INTO EX4.Fee (id, name, age, course, amount) VALUES (1, 'Hoang', '21', 'CNTT', 400000);
+INSERT INTO EX4.Fee (id, name, age, course, amount) VALUES (2, 'Viet', '19', 'DTVT', 320000);
+INSERT INTO EX4.Fee (id, name, age, course, amount) VALUES (3, 'Thanh', '18', 'KTDN', 400000);
+INSERT INTO EX4.Fee (id, name, age, course, amount) VALUES (4, 'Nhan', '19', 'CK', 450000);
+INSERT INTO EX4.Fee (id, name, age, course, amount) VALUES (5, 'Huong', '20', 'TCNH', 500000);
+INSERT INTO EX4.Fee (id, name, age, course, amount) VALUES (6, 'Huong', '21', 'DTVT', 200000);
+
 SELECT *
 FROM Fee
 WHERE name = 'Huong';
